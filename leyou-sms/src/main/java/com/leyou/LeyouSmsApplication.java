a0fcb506e5904dca19e13ff184478245d974175c
wh <1281947642@qq.com>
2020-01-02 15:35:54 +0800

    短信微服务

---
+++ leyou-sms/src/main/java/com/leyou/LeyouSmsApplication.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * <code>LeyouSmsApplication</code>
+ * </p>
+ * 
+ * @author huiwang45@iflytek.com
+ * @description
+ * @date 2020/01/02 15:10
+ */
+@SpringBootApplication
+public class LeyouSmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouSmsApplication.class, args);
+    }
+}
---
+++ leyou-sms/src/main/java/com/leyou/sms/listener/SmsListener.java
@@ -0,0 +1,63 @@
+package com.leyou.sms.listener;
+
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.leyou.sms.config.SmsProperties;
+import com.leyou.sms.utils.SmsUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * <code>SmsListener</code>
+ * </p>
+ * 
+ * @author huiwang45@iflytek.com
+ * @description
+ * @date 2020/01/02 15:24
+ */
+@Component
+@EnableConfigurationProperties(SmsProperties.class)
+public class SmsListener {
+
+    @Autowired
+    private SmsUtils smsUtils;
+
+    @Autowired
+    private SmsProperties prop;
+
+    /**
+     * 发送短息
+     * @description
+     * @author huiwang45@iflytek.com
+     * @date 2020/01/02 15:27
+     * @param msg {phone 手机号 code 验证码}
+     * @return 
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "leyou.sms.queue", durable = "true"),
+            exchange = @Exchange(value = "ly.sms.exchange",
+                                 ignoreDeclarationExceptions = "true"),
+            key = {"sms.verify.code"}))
+    public void sendSms(Map<String, String> msg) throws Exception {
+        if (CollectionUtils.isEmpty(msg)) {
+            // 放弃处理
+            return;
+        }
+        String phone = msg.get("phone");
+        String code = msg.get("code");
+
+        if (StringUtils.isNoneBlank(phone) || StringUtils.isNoneBlank(code)) {
+            // 发送消息
+            SendSmsResponse resp = this.smsUtils.sendSms(phone, code, prop.getSignName(), prop.getVerifyCodeTemplate());
+        }
+    }
+}
