59fb3bc608f8b2d53d198d21deeaeebf9019b4e8
wh <1281947642@qq.com>
2020-01-02 10:12:32 +0800

    用户微服务

--- leyou-user/leyou-user-interface/src/main/java/com/leyou/user/pojo/User.java
+++ leyou-user/leyou-user-interface/src/main/java/com/leyou/user/pojo/User.java
@@ -40,4 +40,52 @@ public class User {
      */
     @JsonIgnore
     private String salt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
 }
